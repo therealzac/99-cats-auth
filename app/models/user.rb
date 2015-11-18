e483024e417f9688194991fbc346a10322f7199e
App Academy Student <student@appacademy.io>
2015-11-18 13:55:54 -0800

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :user_name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :generate_session_token
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+
+    return nil if user.nil?
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+  end
+
+  def generate_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.is_password?(password)
+  end
+
+end
